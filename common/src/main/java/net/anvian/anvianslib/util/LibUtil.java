395179cb025fe1f26f34f6c06700e6e8a5e4aaaf
anviaan <andre.carbajal.vargas@gmail.com>
2025-01-06 21:23:24 -0500

    Bump version to 0.6.0 and add config directory generation utility.

--- common/src/main/java/net/anvian/anvianslib/util/LibUtil.java
+++ common/src/main/java/net/anvian/anvianslib/util/LibUtil.java
@@ -2,6 +2,10 @@ package net.anvian.anvianslib.util;
 
 import net.minecraft.SharedConstants;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class LibUtil {
     public static float ticksToSeconds(int cooldown) {
         return (float) cooldown / 20;
@@ -9,5 +13,16 @@ public class LibUtil {
 
     public static String getMinecraftVersion() {
         return SharedConstants.getCurrentVersion().getName();
+    }
+
+    public static void generateConfigPath(String modId, Path configPath) {
+        Path configDir = configPath.resolve(modId);
+        if (Files.notExists(configDir)) {
+            try {
+                Files.createDirectories(configDir);
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to create config directory: " + configDir, e);
+            }
+        }
     }
 }
