8557ddb8ec91be8ba81c0f27f7a93270cd23e0b3
anviaan <andre.carbajal.vargas@gmail.com>
2026-06-02 23:26:26 -0500

    Add NeoForge support and update dependencies for Minecraft 1.20.2

---
+++ neoforge/src/main/java/net/anvian/anvianslib/NeoForgeMod.java
@@ -0,0 +1,10 @@
+package net.anvian.anvianslib;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class NeoForgeMod {
+    public NeoForgeMod() {
+        CommonMod.init();
+    }
+}
